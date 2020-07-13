4fe637ab1cdd8195c07108221938f4cd1ff23e66
Devansh <61735814+devansh-sudo@users.noreply.github.com>
2020-07-13 20:40:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 const express = require ('express');
-const app = express();
+const app = express()
+const mongoose = require('mongoose')
+const morgon = require ('morgan');
+const dotenv = require('dotenv');
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+
+.then(() => console.log('DB connected'))
+
+mongoose.connection.on ('error' , err => {
+  console.log('DB connection error : ${err.message} ');
+
+});
+
+// app.get ("/" , (req, res ) => {
+//     res.send ("Hello world from node js")
+// });
+
+// app.listen(8080);
 const morgan = require ('morgan');
 
 //bring in routes
-const {getPosts} = require('./routes/post');
+//const postContollers = require('./routes/post');
 
-const myOwnMiddleware = (req,res,next) =>{
-    console.log ('middle ware applied');
-    next();
-};
+const postRoutes = require('./routes/post');
 
-//middeware
+
+// const myOwnMiddleware = (req,res,next) =>{
+//           console.log ('middle ware applied');
+//      next();
+//  };
+
+//middeware*
 
 app.use(morgan('dev'));
-app.use(myOwnMiddleware);
+//app.use(myOwnMiddleware);
 
-app.get("/",getPosts );
-const port = 8080;
+app.use("/" ,postRoutes );
+ const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log('a node js API is listening on port : ${port}');
 });
--- post.js
+++ post.js
@@ -1,11 +1,7 @@
+exports. getPosts = (req ,res) => {
+    //res.send("hello world from node js");
+    res.json({
+        posts : [{tittle : "first post"}, {tittle : "second post"}]
+    });
+}; 
 
-
- const getPosts = (req ,res) => {
-    res.send("hello world from node js");
-
-
-};
-
- module.exports = {
-     getPosts
-};
